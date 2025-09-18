8de6416097e04fe808bc1b4c1affefb77c253dfa
gauthierguillaume <gauthier.guillaume@outlook.com>
2025-09-18 14:30:28 +0200

    Add initial project files including CSS and PHP structure

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/host.php');
+include($_SERVER['DOCUMENT_ROOT'] . '/blocks/header.php');
+?>
+
+<div class="welcome flexCol justifyCenter alignCenter">
+    <h2 class="textCenter">
+        <span>BibliHome</span></br>
+    </h2>
+</div>
+
+<?php
+include($_SERVER['DOCUMENT_ROOT'] . '/blocks/footer.php');
+?>
