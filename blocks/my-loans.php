4c48cf336d09cf2f0b0b39595442213ed11954d1
gauthierguillaume <gauthier.guillaume@outlook.com>
2025-10-03 09:14:03 +0200

    Add my-loans section with styles and integrate into main page

---
+++ blocks/my-loans.php
@@ -0,0 +1,39 @@
+<section class="my-loans container flex-col">
+    <h2>MES LIVRES EMPRUNTES</h2>
+    <div class="my-loans-header flex-row jc-space-between ai-center">
+        <img src="/assets/fo/img/icons/playful list.png" alt="">
+        <p>Titre du livre</p>
+        <p>Auteur</p>
+        <p>État</p>
+        <p>A rendre le</p>
+        <p>Actions</p>
+        <p>plus...</p>
+    </div>
+
+    <div class="my-loans-content">
+        <details>
+            <summary class="flex-row jc-space-between ai-center">
+                <img src="../assets/fo/img/books/le-silmarillion.png" alt="">
+                <p>Le silmarillionfgfdgdfgdgdfgdfgdfgdfgdgdgf</p>
+                <p>J.R.R. Tolkien</p>
+                <div class="book-shipping">
+                    <p>Livré</p>
+                    <p>04/20/2069</p>
+                </div>
+                <p>04/20/2069</p>
+                <div class="book-shipping flex-col">
+                    <div class="return-book flex-row ai-center">
+                        <img src="../assets/fo/img/icons/playful button.png" alt="">
+                        <p>Rendre</p>
+                    </div>
+                    <div class="lost-report flex-row ai-center">
+                        <img src="../assets/fo/img/icons/playful button.png" alt="">
+                        <p>Signaler une perte</p>
+                    </div>
+                </div>
+                <img class="playful-dropdown" src="../assets/fo/img/icons/playful dropdown 32.png" alt="BibliHome">
+            </summary>
+            <p>Pour emprunter un livre sur BibliHome, il vous suffit de vous inscrire et de le réserver en ligne.</p>
+        </details>
+    </div>
+</section>
